cc8439918fe5a632ada40bf06fcdc5d39efe8818
nnnLik <rslmakhmudov@gmail.com>
2023-11-29 13:49:27 +0300

    `add`: db connection

---
+++ server/config/database.py
@@ -0,0 +1,25 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    declared_attr,
+    mapped_column,
+    sessionmaker,
+)
+
+from .settings import settings
+
+engine = create_async_engine(settings.database.database_url)
+Session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f"{cls.__name__.lower()}"
+
+    id: Mapped[int] = mapped_column(primary_key=True)

03999b4a8afcb37478f30a72abb934b38f89ee25
nnnLik <rslmakhmudov@gmail.com>
2023-12-01 00:20:54 +0300

    `edit`: entrypoints

--- server/src/main.py
+++ server/src/main.py
@@ -1,9 +1,38 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from config.settings import settings
 
-app = FastAPI(title=settings.server.NAME, **settings.server.fastapi_kwargs)
+from .modules.router import routes
+
+
+def start_application() -> FastAPI:
+    app = FastAPI(
+        title=settings.server.NAME,
+        version=settings.server.VERSION,
+        **settings.server.fastapi_kwargs
+    )
+
+    setup_settings(app)
+
+    return app
+
+
+def setup_settings(app: FastAPI) -> None:
+    origins = ["*"]
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    app.include_router(router=routes)
+
+
+app: FastAPI = start_application()
 
 if __name__ == "__main__":
     uvicorn.run(

82659845fee78351e157722effc04c0f20a3e4ba
nnnLik <rslmakhmudov@gmail.com>
2023-12-17 03:46:44 +0300

    add: mongo client settings

---
+++ server/config/mongo/__init__.py
@@ -0,0 +1,2 @@
+from .base import Base
+from .mongo import mongo_db_connection
--- server/config/mongo/base.py
+++ server/config/mongo/base.py
@@ -1,7 +1,5 @@
-from datetime import datetime
-
 from pydantic import BaseModel
 
 
 class Base(BaseModel):
-    created_at: datetime = datetime.utcnow()
+    pass

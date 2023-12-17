3a6e24349a710c8d6e3ca1932e3cb0aa711ec045
nnnLik <rslmakhmudov@gmail.com>
2023-12-17 03:48:34 +0300

    add: user profile

--- server/src/modules/auth/models/user.py
+++ server/src/modules/auth/models/user.py
@@ -1,10 +1,11 @@
 from datetime import datetime
+from typing import Optional
 import uuid
 
 from fastapi_users_db_sqlalchemy import UUID_ID, SQLAlchemyBaseUserTableUUID
 from fastapi_users_db_sqlalchemy.generics import GUID
 from sqlalchemy import TIMESTAMP, Boolean, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from config.database import Base
 
@@ -13,8 +14,8 @@ class User(SQLAlchemyBaseUserTableUUID, Base):
     id: Mapped[UUID_ID] = mapped_column(GUID, primary_key=True, default=uuid.uuid4)
 
     username: Mapped[str] = mapped_column(String(length=120), nullable=False)
-    email: Mapped[str] = mapped_column(
-        String(length=320), unique=True, index=True, nullable=False
+    email: Mapped[Optional[str]] = mapped_column(
+        String(length=320), unique=True, index=True, nullable=True
     )
 
     hashed_password: Mapped[str] = mapped_column(String(length=1024), nullable=False)

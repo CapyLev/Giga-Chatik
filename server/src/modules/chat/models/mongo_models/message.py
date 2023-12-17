d9dcf6528bbf80e2af5be2fc1f7d4da69053a7d9
nnnLik <rslmakhmudov@gmail.com>
2023-12-17 03:49:03 +0300

    edit: msg pattern in mongo collection

--- server/src/modules/chat/models/mongo_models/message.py
+++ server/src/modules/chat/models/mongo_models/message.py
@@ -2,15 +2,14 @@ from datetime import datetime
 from typing import List
 from bson import ObjectId
 
-from config.mongo.base import Base
+from config.mongo import Base
 
 
 class Message(Base):
     _id: ObjectId
-    user_id: ObjectId
-    content: str  # TODO: implement multiple uploading content, Union[str[msg], list[file]]
-
-
-class MessageStorage(Base):
-    _id: ObjectId
-    messages: List[Message]
+    user_id: str
+    server_id: str
+    content: str
+    timestamp: datetime = datetime.utcnow()
+    id_deleted: bool = False
+    attachments: List[str] = []

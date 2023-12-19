d6638da02817a6a1c159395e34d4948b474297ea
r.makkhmudov <r.makkhmudov@gmail.com>
2023-12-19 17:03:30 +0300

    add: nav bar

--- client/src/components/NavigateBar/index.tsx
+++ client/src/components/NavigateBar/index.tsx
@@ -1,9 +1,17 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import Image from "next/image";
 import styles from "./NavigateBar.module.scss";
 
 const NavigateBar: FC = () => {
+  useEffect(() => {
+    console.log('useEffect выполнен на сервере или клиенте');
+  }, []);
+
   return (
     <div className={styles.navigationContainer}>
+      <div className={styles.homeIcon}>
+        <Image src="/home.svg" alt="Home" width={55} height={55} />
+      </div>
     </div>
   );
 };

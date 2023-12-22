1f9711d2fdab16e3a239cabbc47ec4b37788f558
r.makkhmudov <r.makkhmudov@admitad.com>
2023-12-22 15:20:49 +0300

    edit auth structure

---
+++ client/src/components/modals/SignUp/SignUp.tsx
@@ -0,0 +1,61 @@
+import React, {FC, useState} from "react";
+import styles from "@/components/modals/Modals.module.scss";
+
+interface SignUpProps {
+    closeModal: () => void;
+}
+
+const SignUp: FC<SignUpProps> = ({closeModal}) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = () => {
+        console.log("Email:", email);
+        console.log("Password:", password);
+        // TODO: send request to server yoy
+    };
+
+    const handleContainerClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        e.stopPropagation();
+    };
+    return (
+        <>
+            <div className={styles.overlay} onClick={closeModal}>
+                <div className={styles.modal} onClick={handleContainerClick}>
+          <span className={styles.close} onClick={closeModal}>
+            &times;
+          </span>
+                    <div className={styles.modalContent}>
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={handleEmailChange}
+                            className={styles.input}
+                        />
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={handlePasswordChange}
+                            className={styles.input}
+                        />
+                        <button onClick={handleSubmit} className={styles.submitButton}>
+                            Submit
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default SignUp;

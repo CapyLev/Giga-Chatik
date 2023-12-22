6cfb483a6f4422e301255fde9741b8b284d8a81c
r.makkhmudov <r.makkhmudov@admitad.com>
2023-12-22 15:21:56 +0300

    add: navbar scroll

--- client/src/components/NavigateBar/NavigateBar.tsx
+++ client/src/components/NavigateBar/NavigateBar.tsx
@@ -4,20 +4,51 @@ import { FC } from "react";
 import styles from "./NavigateBar.module.scss";
 import { Routers } from "@/utils/common";
 
-const NavigateBar: FC = () => {
+interface NavigateBarProps {}
+
+const NavigateBar: FC<NavigateBarProps> = ({}) => {
+  // TODO: its just a test example
+  const serversIds = [1, 2, 3, 4, 5, 6];
+
   return (
     <div className={styles.navigationContainer}>
-      <Link href={Routers.HOME}>
-        <div>
-          <Image
-            className={styles.homeIcon}
-            src="/home.svg"
-            alt="Home"
-            width={55}
-            height={55}
-          />
-        </div>
-      </Link>
+      <div className={styles.topSection}>
+        <Link href={Routers.HOME}>
+          <div>
+            <Image
+              className={styles.Icon}
+              src="/home.svg"
+              alt="Home"
+              width={55}
+              height={55}
+            />
+          </div>
+        </Link>
+      </div>
+
+      <div className={styles.line}></div>
+
+      <div className={styles.centerSection}>
+        {serversIds.map((id) => (
+          <div key={id} className={styles.serverIcon}></div>
+        ))}
+      </div>
+
+      <div className={styles.line}></div>
+
+      <div className={styles.bottomSection}>
+        <Link href={Routers.PROFILE}>
+          <div>
+            <Image
+              className={styles.Icon}
+              src="/profile.svg"
+              alt="Home"
+              width={55}
+              height={55}
+            />
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };

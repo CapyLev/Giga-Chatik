c4013cec9e3b60f8001f27d12a45280706a6c600
nnnLik <rslmakhmudov@gmail.com>
2023-12-24 01:44:57 +0300

    edit endpoints prefix

--- server/src/modules/router.py
+++ server/src/modules/router.py
@@ -2,9 +2,10 @@ from fastapi import APIRouter
 
 from .auth.controllers import router as auth_router
 from .chat.controllers import router as chat_router
+from .server.controllers import router as server_router
 
 routes = APIRouter()
 
-
-routes.include_router(auth_router)
-routes.include_router(chat_router)
+routes.include_router(auth_router, prefix="/auth", tags=["auth"])
+routes.include_router(chat_router, prefix="/chat", tags=["chat"])
+routes.include_router(server_router, prefix="/server", tags=["server"])

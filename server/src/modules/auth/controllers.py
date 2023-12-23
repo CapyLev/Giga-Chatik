f7d7fd686552d2981d12d3e15f83da685cf691d0
nnnLik <rslmakhmudov@gmail.com>
2023-12-24 01:45:23 +0300

    move again to cookie  transport

--- server/src/modules/auth/controllers.py
+++ server/src/modules/auth/controllers.py
@@ -2,25 +2,25 @@ from fastapi import APIRouter
 
 
 from .dto.user import UserCreate, UserRead, UserUpdate
-from .settings import auth_backend, fastapi_users
+from .settings import auth_backend, fastapi_users_auth
 
 router = APIRouter()
 
 
 router.include_router(
-    fastapi_users.get_auth_router(auth_backend),
+    fastapi_users_auth.get_auth_router(auth_backend),
     prefix="/auth",
     tags=["auth"],
 )
 
 router.include_router(
-    fastapi_users.get_register_router(UserRead, UserUpdate),
+    fastapi_users_auth.get_register_router(UserRead, UserUpdate),
     prefix="/auth",
     tags=["auth"],
 )
 
 router.include_router(
-    fastapi_users.get_users_router(UserRead, UserCreate),
+    fastapi_users_auth.get_users_router(UserRead, UserCreate),
     prefix="/auth",
     tags=["auth"],
 )

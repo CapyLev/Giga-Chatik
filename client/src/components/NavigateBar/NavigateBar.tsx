25cb55de220bdbb131dbb18cad72c225452c89e7
nnnLik <rslmakhmudov@gmail.com>
2023-12-25 01:44:19 +0300

    add fetching user servers

--- client/src/components/NavigateBar/NavigateBar.tsx
+++ client/src/components/NavigateBar/NavigateBar.tsx
@@ -1,52 +1,50 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import styles from "./NavigateBar.module.scss";
 import { Routers } from "@/utils/common";
+import { fetchServerIcons } from "@/services/navbar.service";
+import { ServerImage } from "@/interfaces/server.interfaces";
+import Icon from "../Icon/Icon";
 
 interface NavigateBarProps {}
 
 const NavigateBar: FC<NavigateBarProps> = ({}) => {
-  // TODO: its just a test example
-  const serversIds = [1, 2, 3, 4, 5, 6];
+  const [servers, setServers] = useState<ServerImage[]>([]);
+
+  useEffect(() => {
+    fetchServerIcons().then((serversResponse) => {
+      setServers(serversResponse.result);
+    });
+  }, []);
 
   return (
     <div className={styles.navigationContainer}>
       <div className={styles.topSection}>
         <Link href={Routers.HOME}>
-          <div>
-            <Image
-              className={styles.Icon}
-              src="/home.svg"
-              alt="Home"
-              width={55}
-              height={55}
-            />
-          </div>
+          <Icon src="home.svg" alt="Home" />
         </Link>
       </div>
 
       <div className={styles.line}></div>
 
       <div className={styles.centerSection}>
-        {serversIds.map((id) => (
-          <div key={id} className={styles.serverIcon}></div>
-        ))}
+        {servers
+          ? servers.map((server) => (
+              <div key={server.id} className={styles.Icon}>
+                <Icon src={server.image} alt={`Server ${server.id}`} />
+              </div>
+            ))
+          : []}
       </div>
 
       <div className={styles.line}></div>
 
       <div className={styles.bottomSection}>
         <Link href={Routers.PROFILE}>
-          <div>
-            <Image
-              className={styles.Icon}
-              src="/profile.svg"
-              alt="Home"
-              width={55}
-              height={55}
-            />
-          </div>
+          <Icon src="/profile.svg" alt="Home" width={55} height={55} />
         </Link>
       </div>
     </div>

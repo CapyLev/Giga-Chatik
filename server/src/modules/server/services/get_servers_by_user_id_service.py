b58fa6a76abd85ab456f919b40886274a41a220e
nnnLik <rslmakhmudov@gmail.com>
2023-12-26 23:35:48 +0300

    refactor by black

--- server/src/modules/server/services/get_servers_by_user_id_service.py
+++ server/src/modules/server/services/get_servers_by_user_id_service.py
@@ -4,11 +4,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from ..dto import ServerImageDTO, UserServerDTO
 from ..repository import get_user_server_repo
 
+
 class GetServersByUserIdService:
     def __init__(self, session: AsyncSession) -> None:
         self.user_server_repo = get_user_server_repo(session)
 
-    async def _collect_user_servers_by_user_id(self, user_id: str) -> List[UserServerDTO]:
+    async def _collect_user_servers_by_user_id(
+        self, user_id: str
+    ) -> List[UserServerDTO]:
         return await self.user_server_repo.get_user_servers_by_user_id(user_id)
 
     async def execute(self, user_id: str) -> List[ServerImageDTO]:

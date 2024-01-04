54a3d615ceb999528c618cb5096a93d740df4355
nnnLik <rslmakhmudov@gmail.com>
2024-01-05 01:19:10 +0300

    edit redis connection manager

---
+++ server/config/redis_conn/redis_manager.py
@@ -0,0 +1,38 @@
+from enum import Enum
+
+from redis.asyncio.client import Redis
+
+from ..settings import settings
+
+
+class RedisSessionType(Enum):
+    MSG_TEMPORARY_STORAGE = 0
+    ACTIVE_CONNECTIONS_STORAGE = 1
+    CACHE_STORAGE = 2
+
+
+class RedisConnectionManager:
+    def __init__(self, session_type) -> None:
+        self.connection = None
+        self.session_type = session_type
+
+    async def __aenter__(self):
+        self.connection = await self._get_redis_session_by_type(self.session_type.value)
+        return self.connection
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        await self.connection.aclose()
+
+    async def _get_redis_session_by_type(
+        self,
+        session_type: int,
+    ) -> Redis:
+        return await Redis(
+            host=settings.redis.HOST,
+            password=settings.redis.PASSWORD,
+            port=settings.redis.PORT,
+            encoding="utf-8",
+            decode_responses=True,
+            db=session_type,
+            protocol=3,
+        )

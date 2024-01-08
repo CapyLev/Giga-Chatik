c4d8c5d8a53c6304a087ea2de549919fbaa90f54
r.makkhmudov <r.makkhmudov@admitad.com>
2024-01-08 14:56:01 +0300

    add verbose text for fields

--- server/src/modules/communication/models/mongo_models/message.py
+++ server/src/modules/communication/models/mongo_models/message.py
@@ -5,7 +5,6 @@ from mongoengine import (
     DateTimeField,
     BooleanField,
     ListField,
-    ObjectIdField,
     UUIDField,
     URLField,
 )
@@ -13,13 +12,14 @@ from config.mongo import BaseDocument
 
 
 class MessageDocument(BaseDocument):
-    _id = ObjectIdField()
-    user_id = UUIDField(required=True, binary=True)
-    server_id = UUIDField(required=True, binary=True)
-    content = StringField(required=True)
-    timestamp = DateTimeField(required=True, default=datetime.now())
-    id_deleted = BooleanField(default=False)
-    attachments = ListField(URLField())
+    user_id = UUIDField(verbose_name="User uuid", binary=True, required=True)
+    server_id = UUIDField(verbose_name="Server uuid", binary=True, required=True)
+    content = StringField(verbose_name="Message content", required=True)
+    timestamp = DateTimeField(
+        verbose_name="Timestamp", default=datetime.now(), required=True
+    )
+    id_deleted = BooleanField(verbose_name="Message is deleted", default=False)
+    attachments = ListField(URLField(verbose_name="Images/Audio/... urls"))
 
     meta = {
         "indexes": [

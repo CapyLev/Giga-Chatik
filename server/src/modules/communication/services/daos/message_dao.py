0e66a956c776640e77e7bfa2f2ef248a9e436000
nnnLik <rslmakhmudov@gmail.com>
2024-01-11 23:31:36 +0300

    add: fetch last chunk of msg from redis

--- server/src/modules/communication/services/daos/message_dao.py
+++ server/src/modules/communication/services/daos/message_dao.py
@@ -1,4 +1,6 @@
-from typing import List, Optional
+from typing import AsyncGenerator
+
+from src.modules.redis import RedisConnectionManager, RedisSessionType
 from src.modules.communication.dto.message_dto import MessageDTO
 
 
@@ -6,8 +8,15 @@ class MessageDAO:
     MSG_COUNT_TO_FETCH = 300
 
     async def collect_messages(
-        self, last_msg_id: Optional[str] = None
-    ) -> List[MessageDTO]:
-        # FIXME: скорее всего если я хочу сделать так как указал в коменте таски
-        # нужно будет изменить отправляемый обьект на Dict где ключи будут id сообщений
-        ...
+        self,
+        server_id: str,
+    ) -> AsyncGenerator[MessageDTO, None]:
+        server_key = f"server:{server_id}"
+        async with RedisConnectionManager(
+            RedisSessionType.MSG_TEMPORARY_STORAGE
+        ) as conn:
+            messages = await conn.hgetall(server_key)
+
+        for message_json in messages.values():
+            message_data = MessageDTO.model_validate_json(message_json)
+            yield message_data
--- server/src/modules/communication/services/send_messages_on_connect_service.py
+++ server/src/modules/communication/services/send_messages_on_connect_service.py
@@ -1,10 +1,16 @@
 from fastapi import WebSocket
+
 from .daos.message_dao import MessageDAO
+from .message_to_client_service import MessageToClientService
 
 
 class SendMessagesOnConnectService:
-    # TODO: добавить глобальный сервис по отправке сообщений в
-    # который будет отправляться BaseModel а он будет сериализовать и помещать в вебсокет
-    async def execute(self, websocket: WebSocket, message_dao: MessageDAO) -> None:
-        messages = message_dao.collect_messages()
-        await websocket.send_text(messages)
+    def __init__(
+        self, message_dao: MessageDAO, message_to_client_service: MessageToClientService
+    ) -> None:
+        self._message_dao = message_dao
+        self._message_to_client_service = message_to_client_service
+
+    async def execute(self, websocket: WebSocket, server_id: str) -> None:
+        async for message in self._message_dao.collect_messages(server_id):
+            await self._message_to_client_service.execute(websocket, message)
